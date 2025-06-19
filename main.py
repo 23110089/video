005d9c8b9ca93914ff23e126ca94aa9bb85046e5
23110089 <152385695+23110089@users.noreply.github.com>
2025-06-19 16:03:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-from threading import Thread
+from requests import get
 from fastapi import FastAPI, Query
-from os import popen
+from fastapi.responses import JSONResponse
+import subprocess, json, os
 
-# def run(): import run
-# Thread(target=run).start()
+os.system('apt install ffmpeg -y')
+
+def get_video_duration(url):
+    cmd = [
+        'ffprobe',
+        '-v', 'quiet',
+        '-print_format', 'json',
+        '-show_format',
+        url
+    ]
+    try:
+        result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        metadata = json.loads(result.stdout)
+        duration = float(metadata['format']['duration'])
+        return duration
+    except Exception as e:
+        print("Lỗi khi lấy thời lượng video:", e)
+        return None
 
 app = FastAPI()
 @app.get("/")
 def home(): return 'Success'
 
-@app.get("/su")
-def check(cmd: str = Query(...)): return popen(cmd).read()
+@app.get("/video")
+def check(link: str = Query(...)):
+    try:
+        return JSONResponse(content={'length': get_video_duration(link)}, status_code=200)
+    except Exception as e:
+        return JSONResponse(content={'error': str(e)}, status_code=500)
