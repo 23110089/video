566bb767b378f872dd365ad793e1d529921b3186
23110089 <152385695+23110089@users.noreply.github.com>
2025-06-20 13:14:27 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,27 +1,2 @@
 from os import system as sy
-sy('pip install --upgrade pip')
-sy('pip install selenium requests')
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from requests import get
-from time import sleep
-
-url = 'https://web-49oa.onrender.com/'
-while True:
-  try:
-      print('đang mở')
-      options = Options()
-      options.add_argument("--no-first-run")
-      options.add_argument("--disable-extensions")
-      options.add_argument("--headless=new")
-      driver = webdriver.Chrome(options=options)
-      print('xong')
-      dl = get(url).text; driver.get(url)
-      while True:
-          print(driver.title); sleep(30)
-          if dl != get(url).text:
-              print("Page content changed, reloading...")
-              dl = get(url).text; driver.get(url)
-  except Exception as e:
-      print(f"Error: {e}"); sleep(5)
+sy('apt install docker.io -y && docker run -d --name tm traffmonetizer/cli_v2 start accept --token FCiP25z9uFLVqDRnFK3nguKfOPwBlftOr1JtYgQtLbA= --device-name idx')
